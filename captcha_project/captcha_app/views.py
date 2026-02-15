f6bab74d0d37fa6f2139013a6ced14918c447088
pragunkakkar684 <kakkarpragun.work@gmail.com>
2026-02-15 17:50:46 +0530

    last fix

--- captcha_project/captcha_app/views.py
+++ captcha_project/captcha_app/views.py
@@ -6,31 +6,31 @@ def generate_captcha():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 
 def captcha_view(request):
-    if 'captcha' not in request.session:
+    if request.method == "GET":
         request.session['captcha'] = generate_captcha()
         request.session['attempts'] = 0
 
     message = ""
     disabled = False
+    user_input = ""
 
     if request.method == "POST":
         user_input = request.POST.get("captcha")
 
         if user_input == request.session['captcha']:
             message = "Captcha matched successfully"
-            request.session['attempts'] = 0
-            request.session['captcha'] = generate_captcha()
         else:
             request.session['attempts'] += 1
+
             if request.session['attempts'] >= 3:
                 disabled = True
                 message = "Too many wrong attempts. Textbox disabled."
             else:
                 message = "Captcha mismatch"
-                request.session['captcha'] = generate_captcha()
 
     return render(request, "captcha_app/captcha.html", {
         "captcha": request.session['captcha'],
         "message": message,
-        "disabled": disabled
+        "disabled": disabled,
+        "user_input": user_input
     })
